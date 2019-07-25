ebdc52b5000b2924d12aa4bfac004a17cf6c4366
teamDBSD <griddic@mail.ru>
2019-07-25 15:41:05 +0300

    Added FileWriter

---
+++ src/main/java/com/acme/edu/AppTest.java
@@ -0,0 +1,16 @@
+package com.acme.edu;
+
+/**
+ * Created by Java_5 on 23.07.2019.
+ */
+public class AppTest {
+    public static void main(String[] args) throws Exception{
+        Logger.log(123);
+        Logger.log(13);
+        Logger.log("asd");
+        Logger.log("asd");
+        Logger.log("asd");
+        Logger.log(12);
+        Logger.close();
+    }
+}
